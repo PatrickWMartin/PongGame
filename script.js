4742030a024bfdbb00720ad2bda94157f6a8edcc
Patrick Martin <patrickwaltermartin@gmail.com>
2024-06-13 20:52:10 -0300

    Add drawing a simple stationary ball

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//canvas init 
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 const height = canvas.height;
@@ -5,3 +6,10 @@ const width = canvas.width;
 
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, width, height);
+
+//ball logic
+ctx.fillStyle = "white";
+ctx.arc(50, 50, 5, 0, 2 * Math.PI);
+ctx.fill();
+
+
