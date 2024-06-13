edf9cf9e844f7f5243ca299406f9bbce0af739c0
Patrick Martin <patrickwaltermartin@gmail.com>
2024-06-13 20:47:26 -0300

    Pong game background

---
+++ script.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+const height = canvas.height;
+const width = canvas.width;
+
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, width, height);
