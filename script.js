efccf615f884d93f926492b00d9f787f20e25f13
Patrick Martin <patrickwaltermartin@gmail.com>
2024-06-13 21:00:54 -0300

    Add ball object
    Created a ball object to track relevent information about the ball. This way the code is cleaner and more maintainable

--- script.js
+++ script.js
@@ -4,14 +4,23 @@ const ctx = canvas.getContext("2d");
 const height = canvas.height;
 const width = canvas.width;
 
-const draw = function (){
+//create ball
+const ball = {
+    size: 5,
+    xPos: 50,
+    yPos: 50,
+    xSpeed: 2,
+    ySpeed: 2,
+};
+
+const draw = function (ball){
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, width, height);
 
     ctx.fillStyle = "white";
-    ctx.arc(50, 50, 5, 0, 2 * Math.PI);
+    ctx.arc(ball.xPos, ball.yPos, ball.size, 0, 2 * Math.PI);
     ctx.fill();
 }
 
-draw();
+draw(ball);
 
